e875e97088d80da05aafc27b0827ddc932ef59eb
Youssef Waleed <yoyo.kotlin@gmail.com>
2026-05-29 14:26:14 +0300

    analytics added

---
+++ analytics.js
@@ -0,0 +1,10 @@
+const script = document.createElement('script');
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-RHSQ7HYJYZ";
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { window.dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-RHSQ7HYJYZ');
